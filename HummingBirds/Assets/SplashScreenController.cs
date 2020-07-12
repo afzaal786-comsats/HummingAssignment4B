bc71afa33b22e4542bd4cc36c8df31cda5f4e937
Afzaal <65043912+afzaal786-comsats@users.noreply.github.com>
2020-07-13 00:31:56 +0500

    Final Commit
    Final Commit

---
+++ HummingBirds/Assets/SplashScreenController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SplashScreenController : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ToSplashOne());
+    }
+
+    IEnumerator ToSplashOne()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(1);
+    }
+}
